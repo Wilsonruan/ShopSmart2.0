f33568059cfe5d10a2244016719d2e7b7c95ec83
Wilson Ruan <wilsonruan23@gmail.com>
2020-12-29 21:28:18 -0500

    ints cart button

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function CartScreen(props) {
+  const productID = props.match.params.id;
+  const qty = props.location.search 
+  ? Number(props.location.search.split('=')[1])
+  : 1;
+  return (
+    <div>
+      <h1>Cart Screen</h1>
+      <p>Add to Cart : ProductID {productID} Qty: {qty}</p>
+    </div>
+  )
+}
