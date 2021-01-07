b52ae4fcdf18902101589351b96784572c77200c
Wilson Ruan <wilsonruan23@gmail.com>
2021-01-06 23:48:54 -0500

    complete signin

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,12 +5,25 @@ import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers';
+import { userSigninReducer } from './reducers/userReducers';
 
-const initialState = {};
+const initialState = {
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
+  cart: {
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+  },
+};
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userSignin: userSigninReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
